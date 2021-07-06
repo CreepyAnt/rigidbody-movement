734b0aa9ff303f15a3e0a437263a7a9b968fe7c1
CreepyAnt <86672124+CreepyAnt@users.noreply.github.com>
2021-07-06 21:16:30 +0530

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,48 +1,79 @@
 // SUPER MESSY RIGIDBODY BASED MOVEMENT BY ME, ANT!
-// This Code is not optimized, so if you wanna use this code, DONT!
+// ALSO, A LITTLE-BIT OF CODE IS STOLEN FROM DANI AND PLAI :)
+
+// THE CODE ISN'T FULLY OPTIMIZED SO IF THERE IS ANY PROBLEM, ITS BECAUSE I WAS TOO LAZY TO FIX THAT 
+// FEEL FREE TO CONTRIBUTE TO THE CODE
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float playerHeight = 2f;
     
+    [Header("Movement")]
     public float moveSpeed = 15;
     public float jumpForce = 7;
+    public float slideForce = 800;
     float horizontalMove;
     float verticalMove;
 
-    public bool isGrounded;
-    public bool headButting;
-    
-    public LayerMask whatisGround;
-    public Transform groundcheck;
-    public Transform ceilingcheck;
+    [Header("Multipliers")]
+    public float movementMultiplier = 10f;
     public float airMultiplier = 0.4f;
-
-
-    public Transform orientation;
-
+    
+    [Header("Drag")]
     public float groundDrag = 6f;
     public float airDrag = 2f;
-    public float movementMultiplier = 10f;
+    public float crouchDrag = 1f;
+
+    [Header("Checks")]
+    public Transform groundcheck;
+    public Transform ceilingcheck;
+
+    [Header("assignables")]
+    public LayerMask whatisGround;
+    public Transform orientation;
+    
+    [Header("Bools")]
+    public bool isGrounded;
+    public bool headButting;
+    public bool isCrouching;
+    
 
     Vector3 playerScale;
     Vector3 crouchScale = new Vector3 (1f,0.5f,1f);
-    
 
+    RaycastHit slopeHit;
+    bool onSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.5f))
+        {
+            if (slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+        }else
+        {
+            return false;
+        }
+        return false;
+    }
+    
     Rigidbody rb;
 
     Vector3 moveDirection;
+    Vector3 slopeDirection;
    
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
     }
     
     void Start()
     {
+        rb.freezeRotation = true;
         playerScale = transform.localScale;
     }
 
@@ -50,15 +81,21 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {   
         isGrounded = Physics.CheckSphere(groundcheck.position, 0.2f, whatisGround);
-        headButting = Physics.Raycast(ceilingcheck.position, Vector3.up,2.25f, whatisGround);
+        headButting = Physics.Raycast(ceilingcheck.position, Vector3.up, 1f);
         input();
+
+        if (transform.position.y < -25)
+        {
+            transform.position = new Vector3(0,15,0);
+        }
+
+        slopeDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
     }
 
     void FixedUpdate()
     {   
         controlDrag();
         movement();
-
     }
 
     void input()
@@ -79,38 +116,55 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Crouch"))
         {
             startCrouch();
+            isCrouching = true;
+            
         }else if (Input.GetButtonUp("Crouch") && !headButting)
         {
             StopCrouch();
+            isCrouching = false;
+
         }
     }
 
-    // Methods
     
     void movement()
     {
-        if (isGrounded)
+        if (isGrounded && !isCrouching && !onSlope())
         {
             rb.AddForce(moveDirection * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        
+        }else if (isGrounded && onSlope())
+        {
+            rb.AddForce(slopeDirection * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        
         }else if (!isGrounded)
         {
             rb.AddForce(moveDirection * moveSpeed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
+        }
+
+        if (onSlope() && isCrouching)
+        {
+            rb.AddForce(Vector3.down * 300);
         }
     }
             
         
 
     void jump()
-    { 
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        
+    {   
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);    
     }
 
-
-    void startCrouch()
+   void startCrouch()
     {
+        
         transform.localScale = crouchScale;
         transform.position = new Vector3 (transform.position.x, transform.position.y - 0.5f, transform.position.z);
+        if (rb.velocity.magnitude > 10 && isGrounded)
+        {
+            rb.AddForce(orientation.transform.forward * slideForce * 10);
+        }
     }
     void StopCrouch()
     {
@@ -122,10 +176,13 @@ public class PlayerMovement : MonoBehaviour
         if (isGrounded)
         {
             rb.drag = groundDrag;
-        }else
+        }else if (isCrouching)
+        {
+            rb.drag = crouchDrag;
+        }else if (!isGrounded)
         {
             rb.drag = airDrag;
         }
     }
-
 }
+
